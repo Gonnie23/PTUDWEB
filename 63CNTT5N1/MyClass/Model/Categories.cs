7b0c4a1cb7a6873eacdcfe4627ac0b3704fe9555
Y Gôn Niê <96051316+Gonnie23@users.noreply.github.com>
2023-10-06 19:44:35 +0700

    My Third commit

--- 63CNTT5N1/MyClass/Model/Categories.cs
+++ 63CNTT5N1/MyClass/Model/Categories.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyClass.Model;
 
 namespace MyClass.Model
 {
@@ -14,18 +15,28 @@ namespace MyClass.Model
         [Key]
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Tên loại SP")]
         public string Name { get; set; }
+        [Display(Name = "Tên loại rút gọn")]
         public string Slug { get; set; }
         public int ParentID { get; set; }
+        [Display(Name = "Tên loại SP")]
         public int? Order { get; set; }
         [Required]
+        [Display(Name = "Tên loại SP")]
         public string MetaDesc { get; set; }
         [Required]
+        [Display(Name = "Tên loại SP")]
         public string MetaKey { get; set; }
+        [Display(Name = "Tên loại SP")]
         public DateTime CreateAt { get; set; }
+        [Display(Name = "Tên loại SP")]
         public int CreateBy { get; set; }
+        [Display(Name = "Ngày cập nhật bởi ")]
         public DateTime UpdateAt { get; set; }
+        [Display(Name = "Ngày cập nhật")]
         public int UpdateBy { get; set; }
+        [Display(Name = "Trạng thái")]
         public int Status { get; set; }
     }
 
